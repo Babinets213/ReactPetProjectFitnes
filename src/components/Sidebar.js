39bb9a43042235b7a0cb8df15f5d8695921dab26
Babinets213 <babinets.ruslan@student.uzhnu.edu.ua>
2025-10-01 18:00:55 +0300

    Fix bags

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import './styles/Sidebar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faRunning, faChartBar, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-
+import React from "react";
+import "./styles/Sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faRunning,
+  faChartBar,
+  faCog,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { SiNeptune } from "react-icons/si";
 
 function Sidebar() {
@@ -15,18 +20,23 @@ function Sidebar() {
         <ul>
           <li>
             <FontAwesomeIcon icon={faHome} className="fa-icon" />
+            <span>Home</span>
           </li>
           <li>
             <FontAwesomeIcon icon={faRunning} className="fa-icon" />
+            <span>Workout</span>
           </li>
           <li>
             <FontAwesomeIcon icon={faChartBar} className="fa-icon" />
+            <span>Progress</span>
           </li>
           <li>
             <FontAwesomeIcon icon={faCog} className="fa-icon" />
+            <span>Settings</span>
           </li>
           <li>
             <FontAwesomeIcon icon={faSignOutAlt} className="fa-icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </nav>
